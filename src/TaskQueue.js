838adb987fe8902242a812f6edd22a8ffe557f75
Arthur <65594296+InTheDarkAlone@users.noreply.github.com>
2021-03-30 15:12:44 +0500

    Шагиахметов Артур Эдуардович
    1-2 task

--- src/TaskQueue.js
+++ src/TaskQueue.js
@@ -1,4 +1,58 @@
-const TaskQueue = function() {
+class TaskQueue {
+    constructor(props) {
+        this.tasks = [];
+        this.running = false;
+    }
+
+    push (run, dispose, duration) {
+        if (duration === undefined || duration === null) {
+            this.tasks.push({runAndContinue: run, dispose});
+        } else {
+            this.tasks.push({
+                runAndContinue: (continuation) => {
+                    run();
+                    setTimeout(() => {
+                        continuation();
+                    }, duration);
+                },
+                dispose
+            });
+        }
+        this.#runNextTask(this);
+    }
+
+    continueWith (action) {
+        this.push(action, null, 0);
+    };
+
+    #runNextTask(taskQueue) {
+        if (taskQueue.running || taskQueue.tasks.length === 0) {
+            return;
+        }
+        taskQueue.running = true;
+        const task = taskQueue.tasks.shift();
+
+        if (task.runAndContinue) {
+            setTimeout(() => {
+                task.runAndContinue(() => {
+                    task.dispose && task.dispose();
+                    taskQueue.running = false;
+
+                    setTimeout(() => {
+                        this.#runNextTask(taskQueue);
+                    });
+                });
+            }, 0);
+        }
+        else {
+            this.#runNextTask(taskQueue);
+        }
+    }
+}
+
+export default TaskQueue;
+
+/*const TaskQueue = function() {
     function TaskQueue() {
         this.tasks = [];
         this.running = false;
@@ -50,6 +104,6 @@ const TaskQueue = function() {
     }
 
     return TaskQueue;
-}();
+}();*/
 
-export default TaskQueue;
+
--- src/index.js
+++ src/index.js
@@ -28,30 +28,49 @@ function getCreatureDescription(card) {
 }
 
 
-
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card {
+    constructor() {
+        super("Мирная утка", 2);
+    }
+
+    quacks() {
+        console.log('quack')
+    };
+
+    swims() {
+        console.log('float: both;')
+    };
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor() {
+        super("Пес-бандит", 3);
+    }
 }
 
-
-// Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
+];
+const banditStartDeck = [
+    new Dog(),
 ];
 
-// Колода Бандита, верхнего игрока.
-const banditStartDeck = [
-    new Card('Бандит', 3),
-];
+// // Колода Шерифа, нижнего игрока.
+// const seriffStartDeck = [
+//     new Card('Мирный житель', 2),
+//     new Card('Мирный житель', 2),
+//     new Card('Мирный житель', 2),
+// ];
+//
+// // Колода Бандита, верхнего игрока.
+// const banditStartDeck = [
+//     new Card('Бандит', 3),
+// ];
 
 
 // Создание игры.
