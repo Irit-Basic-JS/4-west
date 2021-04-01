faa2e83856c22bc9c0d94a5375e134d88870819d
murrnyaka <murrrnyaka@mail.ru>
2021-04-01 21:08:16 +0500

    1,2,3

--- src/index.js
+++ src/index.js
@@ -13,6 +13,18 @@ function isDog(card) {
     return card instanceof Dog;
 }
 
+class Creature extends Card {
+    constructor(name, maxPower, image) {
+        super(name, maxPower, image);
+    }
+
+    getDescriptions() {
+        return [
+            getCreatureDescription(this), ...super.getDescriptions()
+        ]
+    }
+
+}
 // Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
@@ -27,30 +39,36 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Duck extends Creature{
+    constructor() {
+        super('Мирная утка', 2);
+    }
 
+    quacks() {
+        console.log('quack');
+    };
 
-// Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+    swims() {
+        console.log('float: both;')
+    };
 }
 
-
-// Основа для собаки.
-function Dog() {
-}
-
+class Dog extends Creature{
+    constructor() {
+        super('Пес-бандит', 3);
+    }
+};
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
