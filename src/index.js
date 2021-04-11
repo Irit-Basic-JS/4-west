95bf9564e507e29583ae880ece5dbdc6c2c0bc4b
Белов Артём <theflash.0320@gmail.com>
2021-04-11 15:52:14 +0500

    master

--- src/index.js
+++ src/index.js
@@ -27,30 +27,147 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
+class Creature extends Card {
+    constructor(name, maxPower) {
+        super(name, maxPower);
+    }
+    
+    getDescriptions() {
+        let first = getCreatureDescription(this);
+        let second = super.getDescriptions();
+        return [first, second];
+    }
+}
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature {
+    constructor(name = 'Мирная утка', maxPower = 2) {
+        super(name, maxPower);
+    }
+    
+    quacks() {
+        console.log('quack')
+    }
+
+    swims() {
+        console.log('float: both;')
+    }
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor(name = "Пес-бандит", maxPower = 3) {
+        super(name, maxPower);
+    }
+}
+
+class Trasher extends Dog {
+    constructor(name = "Громила", maxPower = 5) {
+        super(name, maxPower);
+    }
+
+    modifyTakenDamage(value, fromCard, gameContext, continuation) {
+        this.view.signalAbility(() => {
+            super.modifyTakenDamage(value - 1, fromCard, gameContext, continuation);
+        });
+    }
+    
+    getDescriptions() {
+        let first = 'Получает на 1 меньше урона';
+        return [first, super.getDescriptions()];
+    }
+}
+
+class Gatling extends Creature {
+    constructor(name = 'Гатлинг', maxPower = 6) {
+        super(name, maxPower);
+    }
+
+    attack(gameContext, continuation) {
+        const taskQueue = new TaskQueue();
+
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+        const cards = gameContext.oppositePlayer.table;
+        for (const card of cards) {
+            if (cards) {
+                taskQueue.push(onDone => {
+                    this.dealDamageToCreature(2, card, gameContext, onDone);
+                });
+            } else {
+                taskQueue.push(onDone => this.dealDamageToPlayer(1, gameContext, onDone));
+            }
+        }
+        taskQueue.continueWith(continuation);
+    }
+}
+
+class Lad extends Dog {
+    constructor(name = 'Братки', maxPower = 2) {
+        super(name, maxPower);
+    }
+
+    static getInGameCount() { 
+        return Lad.inGameCount || 0; 
+    }
+    
+    static setInGameCount(value) { 
+        Lad.inGameCount = value; 
+    }
+
+    static getBonus() {
+        return Lad.getInGameCount() * (Lad.getInGameCount() + 1) / 2;
+    }
+
+    doAfterComingIntoPlay(gameContext, continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() + 1); 
+        super.doAfterComingIntoPlay(gameContext, continuation);       
+    }
+        
+    doBeforeRemoving(continuation) {
+        Lad.setInGameCount(Lad.getInGameCount() - 1);
+        super.doBeforeRemoving(continuation);
+    }
+
+    modifyDealedDamageToCreature(value, toCard, gameContext, continuation) {
+        super.modifyDealedDamageToCreature(value + Lad.getBonus(), toCard, gameContext, continuation);
+    }
+
+    modifyTakenDamage(value, toCard, gameContext, continuation) {
+        super.modifyTakenDamage(value - Lad.getBonus(), toCard, gameContext, continuation);
+    }
+}
+
+class PseudoDuck extends Dog {
+    constructor(name = 'Псевдоутка', maxPower = 3) {
+        super(name, maxPower);
+    }
+    
+    quacks() {
+        console.log('quack')
+    }
+
+    swims() {
+        console.log('float: both;')
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck()
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Lad(),
+    new PseudoDuck(),
+    new Lad()
 ];
 
 
