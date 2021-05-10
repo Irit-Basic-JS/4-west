82c2280193024fa56435d906bcf7c3e7e8fbdf1e
alexxykv <alexxonov@gmail.com>
2021-05-11 00:11:02 +0500

    Tasks 1-3

--- src/index.js
+++ src/index.js
@@ -28,29 +28,50 @@ function getCreatureDescription(card) {
 }
 
 
+class Creature extends Card {
+    getDescriptions() {
+        return [
+            getCreatureDescription(this),
+            ...super.getDescriptions(),
+        ];
+    }
+}
+
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Creature {
+    constructor() {
+        super('Мирная утка', 2);
+    }
+
+    quacks() {
+        console.log('quack');
+    }
+
+    swims() {
+        console.log('float: both');
+    }
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Creature {
+    constructor() {
+        super('Пес-бандит', 3);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
 
 
