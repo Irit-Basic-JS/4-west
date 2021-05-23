406ab5701be0680c4efd8e035541165e06be3f6b
Aleksander Zverev <aleksander.zverev.lt@gmail.com>
2021-05-23 10:08:33 +0500

    1 - 2 task done

--- src/index.js
+++ src/index.js
@@ -30,29 +30,40 @@ function getCreatureDescription(card) {
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card {
+    constructor() {
+        super("Мирная утка", 2);
+    }
+    
+    quacks() { 
+        console.log('quack');
+    };
+
+    swims() {
+        console.log('float: both;');
+    }
+
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor() {
+        super("Пёс бандит", 3);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck(),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
 ];
-
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
