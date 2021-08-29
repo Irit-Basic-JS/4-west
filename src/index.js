b3e586efef5d21c4aa7daab2c06c99116bc5f74b
philya71828 <philya71828@yandex.ru>
2021-08-29 19:26:56 +0500

    Task done

--- src/index.js
+++ src/index.js
@@ -29,11 +29,21 @@ function getCreatureDescription(card) {
 
 //Класс Creature
 class Creature extends Card
-{
-    getDescriptions()
+{   constructor(name, maxPower, image)
     {
+        super (name, maxPower, image);
+        this._currentPower = maxPower;
+    }
+
+    getDescriptions() {
         return [getCreatureDescription(this), super.getDescriptions()];
     } 
+ 
+    set currentPower(value) {
+        value > this.maxPower ? this._currentPower = this.maxPower : this._currentPower = value;
+    }
+    
+    get currentPower() {return this._currentPower;}
 };
 
 // Основа для утки.
@@ -84,11 +94,17 @@ class Trasher extends Dog
 
     getDescriptions()
     {
-        const rez = super.getDescriptions();
-        rez.unshift('Урон на 1 меньше', '<br><br>');
-        return rez;
+        if (Trasher.prototype.hasOwnProperty('modifyTakenDamage')) 
+        {
+            const rez = super.getDescriptions();
+            rez.unshift('Урон на 1 меньше', '<br><br>');
+            return rez;
+        } else
+        {
+            return super.getDescriptions();   
+        }
     };
-};
+}
 
 // Класс Gatling
 class Gatling extends Creature 
@@ -183,34 +199,142 @@ class Lad extends Dog
     };
 };
 
+// Класс Rogue
+class Rogue extends Creature 
+{
+    constructor(name, maxPower, image)
+    {
+        super(name||'Изгой', maxPower||2, image);
+    }
+
+    // Забирает способности у типа карты противника
+    doBeforeAttack(gameContext, continuation) 
+    {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        if (position < oppositePlayer.table.length)
+        {
+            let objPrototype = Object.getPrototypeOf(oppositePlayer.table[position]);
+            let arrPr = ['modifyDealedDamageToCreature', 'modifyDealedDamageToPlayer', 'modifyTakenDamage'];
+            for (let i = 0; i < arrPr.length; i++)
+                if (objPrototype.hasOwnProperty(arrPr[i]))
+                { 
+                    // добавляем способность изгою, но не в прототип, чтобы не наследовалась
+                    currentPlayer.table[position][arrPr[i]] = objPrototype[arrPr[i]];
+                    // удаляем способность у класса карты соперника
+                    delete objPrototype[arrPr[i]];
+                    // запоминаем, у кого отняли способности
+                    currentPlayer.table[position].takeAway = oppositePlayer.table[position].name;    
+                }       
+            updateView();
+        }
+        continuation();
+    };
+
+    getDescriptions()
+    {
+        let str = 'Отнимаю способности'
+        if (this.takeAway) str = 'Отнял способности у карты \"' + this.takeAway + '\"';
+        const rez = super.getDescriptions();
+        rez.unshift(str, '<br><br>');
+        return rez;
+    };
+}
+
+// Класс Brewer
+class Brewer extends Duck 
+{
+    constructor(name, maxPower, image)
+    {
+        super(name||'Пивовар', maxPower||2, image);
+    }
+    
+    // Раздача "пива"
+    doBeforeAttack(gameContext, continuation) 
+    {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        let arrCard = currentPlayer.table.concat(oppositePlayer.table);
+        for (let i = 0; i < arrCard.length; i++)
+            if (isDuck(arrCard[i])) 
+                arrCard[i].view.signalHeal (() => {
+                    arrCard[i].maxPower++; 
+                    arrCard[i].currentPower += 2;  
+                    arrCard[i].updateView();
+                });         
+        continuation();
+    };
+
+    getDescriptions()
+    {
+        const rez = super.getDescriptions();
+        rez.unshift('Раздаю уткам способности', '<br><br>');
+        return rez;
+    };
+    
+};
+
+// Класс PseudoDuck
+class PseudoDuck extends Dog 
+{
+    constructor(name, maxPower, image)
+    {
+        super(name||'Псевдоутка', maxPower||3, image);
+    }
+
+    quacks() { console.log('quack') };
+    swims() { console.log('float: both;') };
+}
+
+// Класс Nemo
+class Nemo extends Creature 
+{
+    constructor(name, maxPower, image)
+    {
+        super(name||'Немо', maxPower||4, image);
+    }
+
+    // Крадет прототип у карты противника, и теряет свою способность воровать
+    doBeforeAttack(gameContext, continuation) 
+    {
+        const {currentPlayer, oppositePlayer, position, updateView} = gameContext;
+        if (position < oppositePlayer.table.length)
+        {
+            let proto = Object.getPrototypeOf(oppositePlayer.table[position]);
+            Object.setPrototypeOf(this, proto);
+            updateView();
+            this.doBeforeAttack(gameContext, continuation);
+        } else continuation();
+    };
+}
+
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Gatling(),
     new Duck(),
     new Duck(),
-    new Duck(),
-    new Gatling(),
-    new Duck(),
+    new Rogue(),
+    new Rogue(),
+    new Brewer(),
+    new Brewer(),
+    new Nemo(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Dog(),
-    new Dog(),
-    new Trasher(),  
+    new Lad(),
+    new Lad(),
     new Lad(),
     new Dog(),
-    new Lad(),
-    new Dog(),    
+    new Dog(),
+    new PseudoDuck(),
+    new Brewer(),
+    new Brewer(),
 ];
-
 
 // Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
 
 // Глобальный объект, позволяющий управлять скоростью всех анимаций.
-SpeedRate.set(0.5);
+SpeedRate.set(1);
 
 // Запуск игры.
 game.play(false, (winner) => {
