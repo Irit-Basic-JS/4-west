12aeb868fc2e77cdca5def3040d28df6b288170c
AnesVijay <2011andreyka@gmail.com>
2021-10-28 22:28:16 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -30,27 +30,35 @@ function getCreatureDescription(card) {
 
 
 // Основа для утки.
-function Duck() {
-    this.quacks = function () { console.log('quack') };
-    this.swims = function () { console.log('float: both;') };
+class Duck extends Card {
+    constructor(name = "Мирная утка", maxPower = 2) {
+        super(name, maxPower);
+    }
+
+    quacks = function() { console.log ('quack')};
+
+    swims = function () { console.log('float: both;') };
 }
 
 
 // Основа для собаки.
-function Dog() {
+class Dog extends Card {
+    constructor(name = 'Пес-бандит', maxPower = 3){
+        super(name, maxPower);
+    }
 }
 
 
 // Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
-    new Card('Мирный житель', 2),
+    new Duck("добрая утка", 10),
+    new Duck(),
 ];
 
 // Колода Бандита, верхнего игрока.
 const banditStartDeck = [
-    new Card('Бандит', 3),
+    new Dog(),
+    new Dog('генерал Гав', 20),
 ];
 
 
