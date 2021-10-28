9246624ea4e34decfd28bc9f3cf4febd361916db
dclxxxvi <t.aleksandr888@mail.ru>
2021-10-29 04:15:28 +0500

    1-10 done

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import Game from './Game.js';
 import TaskQueue from './TaskQueue.js';
 import SpeedRate from './SpeedRate.js';
 
-// Отвечает является ли карта уткой.
 function isDuck(card) {
     return card && card.quacks && card.swims;
 }
 
-// Отвечает является ли карта собакой.
 function isDog(card) {
     return card instanceof Dog;
 }
 
-// Дает описание существа по схожести с утками и собаками
 function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
@@ -231,7 +228,6 @@ class Nemo extends Creature {
     }
 }
 
-// Колода Шерифа, нижнего игрока.
 const seriffStartDeck = [
     new Nemo(),
 ];
@@ -240,13 +236,10 @@ const banditStartDeck = [
     new Brewer(),
 ];
 
-// Создание игры.
 const game = new Game(seriffStartDeck, banditStartDeck);
 
-// Глобальный объект, позволяющий управлять скоростью всех анимаций.
 SpeedRate.set(3);
 
-// Запуск игры.
 game.play(false, (winner) => {
     alert('Победил ' + winner.name);
 });
